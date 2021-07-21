d8b683e9681da08f391ffe4ce457f3e982cbfcf5
Gonzalo Lamas <lamasgonzaloezequiel@gmail.com>
2021-07-21 18:43:59 -0300

    creando una lista de tareas y sumando nueva con useState

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,8 +1,30 @@
+import { useState } from "react"
+
 export const GifExpertApp = () => {
+
+    /* const categories = ['one punch','samurai x', 'draogn ball'] */
+
+    const [categories, setCategories] = useState(['one punch','samurai x', 'draogn ball'])
+
+    let handleAdd = () => {
+        //setCategories(['hunterxhunter', ...categories])
+        setCategories( cats => ['hunterxhunter', ...cats])
+    }
+
     return(
         <>
             <h2>Gif Expert App</h2>
             <hr></hr>
+
+            <button onClick={handleAdd}>agregar</button>
+
+            <ol>
+            {
+            categories.map( category => {
+                return <li key={category}>{category} </li>
+            })
+            }
+            </ol>
         </>
     )
 }
