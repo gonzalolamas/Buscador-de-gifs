4ad09e379950937e0de981de57b69839d624e6ba
Gonzalo Lamas <lamasgonzaloezequiel@gmail.com>
2021-08-22 00:57:29 -0300

    simulación del submit y restableciendo a vacío

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -30,5 +30,25 @@ describe("Pruebas en AddCategory", () => {
 
     expect(setCategories).not.toHaveBeenCalled();
   })
-  
+
+  test('debe de llamar el setCategories y limpiar la caja de texto', () => {
+    // 1- simular el inputChange
+    
+    const value = 'Hola value'
+    wrapper.find('input').simulate('change', {
+      target: {value}
+    })
+
+    // 2- simular el submit del form
+    wrapper.find('form').simulate('submit', {preventDefault: ()=>{},})
+
+    // 3- setCategories se debe de haber llamado
+    expect(setCategories).toHaveBeenCalled()
+    expect(setCategories).toHaveBeenCalledTimes(1)
+    expect(setCategories).toHaveBeenCalledWith(expect.any(Function))
+
+
+    // 4- el valor del input debe de estar vacio ''
+    expect(wrapper.find('input').text().trim()).toBe('')
+  })
 });
